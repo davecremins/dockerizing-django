766f37a3536e64aae199e8c624a5dcc37bd06d08
Dave Cremins <dave.cremins@wpengine.com>
2017-02-01 23:56:46 +0000

    Added user to model

--- web/docker_django/apps/todo/models.py
+++ web/docker_django/apps/todo/models.py
@@ -1,6 +1,7 @@
 from django.db import models
-
+from django.contrib.auth.models import User
 
 class Item(models.Model):
+    user = models.ForeignKey(User)
     text = models.TextField(blank=False, null=False)
     date_posted = models.DateField(auto_now=True)
