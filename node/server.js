c4dcb9560550f836acbcc0b22991dc748cec5176
Dave Cremins <dave.cremins@wpengine.com>
2017-02-02 22:16:03 +0000

    Remove dead dependencies and simplify node server

--- node/server.js
+++ node/server.js
@@ -1,9 +1,6 @@
-var express = require('express');
-var app = express();
-var server = require('http').createServer(app);
+// Setup express and socket.io
+var server = require('http').createServer(require('express')());
 var io = require('socket.io')(server);
-var redis = require('redis');
-var sub = redis.createClient({host: 'redis'});
 
 var port = 4000;
 server.listen(port, function () {
@@ -13,29 +10,11 @@ server.listen(port, function () {
 io.on('connection', function (socket) {
     console.log('socket connection received');
 });
- 
+
+// Setup redis Pub/Sub bus
+var redis = require('redis');
+var sub = redis.createClient({host: 'redis'});
 sub.subscribe('comms');
-//Grab message from Redis and send to client
 sub.on('message', function(channel, message){        
     console.log(`Message received: ${message}`);    
 });
- 
-//Configure socket.io to store cookie set by Django
-// io.configure(function(){
-//     io.set('authorization', function(data, accept){
-//         if(data.headers.cookie){
-//             data.cookie = cookie_reader.parse(data.headers.cookie);
-//             return accept(null, true);
-//         }
-//         return accept('error', false);
-//     });
-//     io.set('log level', 1);
-// });
- 
-// io.sockets.on('connection', function (socket) {
-//     //Grab message from Redis and send to client
-//     sub.on('message', function(channel, message){
-//         console.log('Message received: ${message}');
-//         socket.send(message);
-//     });
-// });
