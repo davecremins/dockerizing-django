bd852fbf99e4a73d5c358bfe13457763d55d6adc
Dave Cremins <dave.cremins@wpengine.com>
2017-02-06 13:47:05 +0000

    Configure nginx for socket.io

--- node/server.js
+++ node/server.js
@@ -7,8 +7,11 @@ server.listen(port, function () {
    console.log("Express server running on port %s", port);
 });
 
-io.on('connection', function (socket) {
-    console.log('socket connection received');
+io.on('connection', function(socket){
+    console.log("Connection received from client");
+    socket.on('register-identifier', function(clientId) {
+        console.log(`Received client id ${clientId} for socket id ${socket.id}`);
+    });
 });
 
 // Setup redis Pub/Sub bus
