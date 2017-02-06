71512f08b1235b7b6e0ac871df0d30f88e0d7d12
Dave Cremins <dave.cremins@wpengine.com>
2017-02-06 21:59:44 +0000

    Add socket and user id combo to socket manager

--- node/services/socketServer.js
+++ node/services/socketServer.js
@@ -1,9 +1,15 @@
+var socketManager = require('../lib/socketMapper.js');
+let addToManager = (socket, id) => {
+    socketManager.addSocketForId(socket, id);
+};
+
 module.exports = (server) => {
     var io = require('socket.io')(server);
     io.on('connection', function(socket){
         console.log("Connection received from client");
         socket.on('register-identifier', function(clientId) {
             console.log(`Received client id ${clientId} for socket id ${socket.id}`);
+            addToManager(socket, clientId);
         });
     });
 
